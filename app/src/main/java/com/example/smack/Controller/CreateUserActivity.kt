9c04ef81765fdc4a3d856fe0fd12feebb92c34fa
Himu Chowdhury <himu.chowdhury01@gmail.com>
2020-06-14 20:38:26 +0600

    create user activity and spinner added

--- app/src/main/java/com/example/smack/Controller/CreateUserActivity.kt
+++ app/src/main/java/com/example/smack/Controller/CreateUserActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.example.smack.R
 import com.example.smack.Services.AuthService
 import kotlinx.android.synthetic.main.activity_create_user.*
@@ -16,6 +17,7 @@ class CreateUserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
+        createSpinner.visibility = View.INVISIBLE
     }
 
     //Random Sign up avatar image for user
@@ -52,16 +54,55 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun createUserClicked(view: View) {
+        enableSpinner(true)
+        val userName = createUserNameText.text.toString()
         val email = createEmailText.text.toString()
         val password = createPasswordText.text.toString()
-        AuthService.registerUser(this, email, password) {complete ->
-            if (complete) {
-                AuthService.loginUser(this, email, password) {loginSuccess ->
-                    if (loginSuccess) {
 
+        if (userName.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()) {
+            AuthService.registerUser(this, email, password) {registerSuccess ->
+                if (registerSuccess) {
+                    AuthService.loginUser(this, email, password) {loginSuccess ->
+                        if (loginSuccess) {
+                            AuthService.createUser(this, userName, email, userAvatar, avatarColor) {creatSuccess ->
+                                if (creatSuccess){
+
+                                    enableSpinner(false)
+                                    finish()
+                                } else {
+                                    errorToast()
+                                }
+                            }
+                        } else {
+                            errorToast()
+                        }
                     }
+                }
+                else {
+                    errorToast()
                 }
             }
         }
+        else {
+            Toast.makeText(this, "Make sure user name, email, and password are filled in.", Toast.LENGTH_LONG).show()
+            enableSpinner(false)
+        }
+
+
     }
+    fun errorToast() {
+        Toast.makeText(this, "Something Went Wrong, Please Try Again.", Toast.LENGTH_LONG).show()
+        enableSpinner(false)
+    }
+    fun enableSpinner(enable: Boolean) {
+        if (enable) {
+            createSpinner.visibility = View.VISIBLE
+        } else {
+            createSpinner.visibility = View.INVISIBLE
+        }
+        createUserBtn.isEnabled = !enable
+        createAvatarImageView.isEnabled = !enable
+        backgroundColorBtn.isEnabled = !enable
+    }
+
 }
